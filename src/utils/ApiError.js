b402dbecaa03555e2639007cbaded084e577697d
Shivam <shivamgarade05@gmail.com>
2026-06-27 12:19:08 +0530

    add explaination of super and stack trace using comment

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -5,6 +5,14 @@ class ApiError extends Error {
         errors = [],
         stack = ""
     ) {
+        // when we extend any class parent class's constructor not automatically called.
+        // so using super we call parent class constructor  
+        // class Error {
+        //     constructor(message){
+        //         this.message = message
+        //     }
+        // } this is how it present there so we need to use super
+        
         super(message)
         this.statusCode = statusCode
         this.data = null
@@ -13,8 +21,10 @@ class ApiError extends Error {
         this.errors = errors
 
         if(stack){
+            // if stack trace available then give it
             this.stack = stack
         }else{
+            // else generate it automatically
             Error.captureStackTrace(this,this.constructor)
         }
     }
