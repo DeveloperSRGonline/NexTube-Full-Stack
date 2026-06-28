c9aa4dd9d27a502196af4fef0eb105ee1e54bdce
Shivam <shivamgarade05@gmail.com>
2026-06-28 21:28:29 +0530

    add comment for understanding

--- src/app.js
+++ src/app.js
@@ -2,7 +2,10 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+// app is express instance
 const app = express();
+
+// cors configuration
 app.use(
     cors({
         origin: process.env.CORS_ORIGIN,
@@ -10,12 +13,14 @@ app.use(
     })
 );
 
+// middleware for parsing json data with limit of 16kb
 app.use(
     express.json({
         limit: "16kb",
     })
 );
 
+// middleware for parsing url encoded data
 app.use(
     express.urlencoded({
         extended: true,
@@ -23,6 +28,7 @@ app.use(
     })
 );
 
+// middleware for serving static files
 app.use(express.static("public"));
 
 // to perform crud operation on cookies
