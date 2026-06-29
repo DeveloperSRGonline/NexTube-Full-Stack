fad452d5b210b273057823051b8e248b5bd00bb6
Shivam <shivamgarade05@gmail.com>
2026-06-29 13:42:43 +0530

    feat: add multer middleware for avatar and coverImage upload in register route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,23 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js"
 
 const router = Router();
 
 // final url will be /users/register
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([
+        {
+            name:"avatar",
+            maxCount:1
+        },
+        {
+            name:"coverImage",
+            maxCount:1
+        }
+    ]),
+    registerUser
+)
 
 // router.route("/login").post()
 
